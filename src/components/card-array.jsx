dcdf4e0bc48600e132fa0460f8f81fb81e3fd569
SKodehode <stiankodehode@gmail.com>
2023-09-01 13:54:24 +0200

    Updated card images.

--- src/components/card-array.jsx
+++ src/components/card-array.jsx
@@ -33,28 +33,28 @@ export const cardArray = [
         timestamp: "2023-09-05 14:00:00",
         title: "Sweet Tooth Satisfaction",
         description: "Savoring a delightful slice of creamy cheesecake topped with fresh berries.",
-        image: ".././assets/images/",
+        image: ".././assets/images/desserts/desserts (1).jpg",
     },
 
     {
         timestamp: "2023-09-06 16:30:00",
         title: "Decadent Chocolate Delight",
         description: "Indulging in a rich, molten lava cake oozing with warm chocolate goodness.",
-        image: ".././assets/images/",
+        image: ".././assets/images/desserts/desserts (2).jpg",
     },
 
     {
         timestamp: "2023-09-07 10:15:00",
         title: "Ice Cream Paradise",
         description: "Cooling off with a double scoop of artisanal ice cream on a hot summer day.",
-        image: ".././assets/images/",
+        image: ".././assets/images/desserts/desserts (3).jpg",
     },
 
     {
         timestamp: "2023-09-08 19:45:00",
         title: "Pastry Perfection",
         description: "Delighting in a flaky, buttery croissant that melts in your mouth.",
-        image: ".././assets/images/",
+        image: ".././assets/images/desserts/desserts (4).jpg",
     },
 
     /* Alcohol Beverages and Drinks */
@@ -62,28 +62,28 @@ export const cardArray = [
         timestamp: "2023-09-09 20:00:00",
         title: "Craft Beer Tasting",
         description: "Sampling a flight of local craft beers, each with its unique flavor profile.",
-        image: ".././assets/images/",
+        image: ".././assets/images/drinks/drinks (1).jpg",
     },
 
     {
         timestamp: "2023-09-10 21:15:00",
         title: "Mixology Masterclass",
         description: "Learning the art of mixing cocktails, from classic martinis to creative concoctions.",
-        image: ".././assets/images/",
+        image: ".././assets/images/drinks/drinks (2).jpg",
     },
 
     {
         timestamp: "2023-09-11 17:30:00",
         title: "Wine and Dine",
         description: "Pairing a fine red wine with a gourmet dinner, enhancing the flavors.",
-        image: ".././assets/images/",
+        image: ".././assets/images/drinks/drinks (3).jpg",
     },
 
     {
         timestamp: "2023-09-12 22:00:00",
         title: "Tropical Escape",
         description: "Sipping on a refreshing piña colada while enjoying the ocean breeze.",
-        image: ".././assets/images/",
+        image: ".././assets/images/drinks/drinks (4).jpg",
     },
 
     /* Nature */
@@ -92,28 +92,28 @@ export const cardArray = [
         timestamp: "2023-09-13 08:00:00",
         title: "Sunrise Hike",
         description: "Witnessing the breathtaking sunrise from a mountaintop, surrounded by nature's beauty.",
-        image: ".././assets/images/",
+        image: ".././assets/images/nature/nature (1).jpg",
     },
 
     {
         timestamp: "2023-09-14 12:45:00",
         title: "Tranquil Forest Retreat",
         description: "Finding serenity in a peaceful forest, with the sound of rustling leaves and birdsong.",
-        image: ".././assets/images/",
+        image: ".././assets/images/nature/nature (2).jpg",
     },
 
     {
         timestamp: "2023-09-15 16:20:00",
         title: "Beachside Bliss",
         description: "Lounging on a pristine beach, with crystal-clear waters and soft sandy shores.",
-        image: ".././assets/images/",
+        image: ".././assets/images/nature/nature (3).jpg",
     },
 
     {
         timestamp: "2023-09-16 19:00:00",
         title: "Lakeside Serenade",
         description: "Kayaking on a tranquil lake, surrounded by majestic mountains and reflections.",
-        image: ".././assets/images/",
+        image: ".././assets/images/nature/nature (4).jpg",
     },
 
     /* Office Culture */
@@ -121,28 +121,28 @@ export const cardArray = [
         timestamp: "2023-09-17 09:00:00",
         title: "Monday Meetings",
         description: "Starting the week with team brainstorming sessions and fresh ideas.",
-        image: ".././assets/images/",
+        image: ".././assets/images/office/office (1).jpg",
     },
 
     {
         timestamp: "2023-09-18 14:30:00",
         title: "Office Celebrations",
         description: "Marking milestones with cake and camaraderie, fostering a positive work environment.",
-        image: ".././assets/images/",
+        image: ".././assets/images/office/office (2).jpg",
     },
 
     {
         timestamp: "2023-09-19 11:45:00",
         title: "Creative Collaborations",
         description: "Collaborating on innovative projects, fueled by creativity and collaboration.",
-        image: ".././assets/images/",
+        image: ".././assets/images/office/office (3).jpg",
     },
 
     {
         timestamp: "2023-09-20 17:15:00",
         title: "Friday Fun Day",
         description: "Unwinding with casual Fridays, games, and laughter in the office.",
-        image: ".././assets/images/",
+        image: ".././assets/images/office/office (4).jpg",
     },
 
     /* Sports */
@@ -150,27 +150,28 @@ export const cardArray = [
         timestamp: "2023-09-21 20:30:00",
         title: "Soccer Showdown",
         description: "Cheering for our favorite team in a thrilling soccer match with friends.",
-        image: ".././assets/images/",
+        image: ".././assets/images/sports/sports (1).jpg",
     },
 
     {
         timestamp: "2023-09-22 18:45:00",
         title: "Tennis Triumph",
         description: "Playing an intense game of tennis, feeling the rush of competition.",
-        image: ".././assets/images/",
+        image: ".././assets/images/sports/sports (2).jpg",
     },
 
     {
         timestamp: "2023-09-23 15:10:00",
         title: "Gym Grind",
         description: "Pushing limits at the gym, striving for personal fitness goals.",
-        image: ".././assets/images/",
+        image: ".././assets/images/sports/sports (3).jpg",
     },
 
     {
         timestamp: "2023-09-24 12:00:00",
         title: "Golf Getaway",
         description: "Teeing off at a scenic golf course, enjoying the calm and precision of the sport.",
+        image: ".././assets/images/sports/sports (4).jpg",
     }]
 
     console.log(cardArray)
